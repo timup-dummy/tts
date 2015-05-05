08ce46012184abfa9641d2abf01f9d53b6e8043d
Tim Upchurch <timup@Tims-MacBook-Pro.local>
2015-05-05 09:20:10 -0400

    classwork 5/5

---
+++ rails/hello/app/models/home.rb
@@ -0,0 +1,15 @@
+class Home
+  
+  def name
+    name = "Tim"
+  end
+  
+  def birthdate
+    birthdate = Date.new(1987,12,1)
+  end
+  
+  def address
+    address = "6 Pinnacle Rd, Durham, NC 27705"
+  end
+  
+end
